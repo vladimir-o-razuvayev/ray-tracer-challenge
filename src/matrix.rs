eff4a431ba4fffd7c0d3075c3290b2bac93e24fe
Vladimir Razuvayev <vladimir.o.razuvayev@gmail.com>
2022-10-02 13:11:47 -0400

    multiply matrix by tuple

--- src/matrix.rs
+++ src/matrix.rs
@@ -1,5 +1,7 @@
 use float_cmp::approx_eq;
 
+use crate::{point::Point, tup, tuple::Tuple, vector::Vector};
+
 #[macro_export]
 macro_rules! mat {
     () => (
@@ -57,6 +59,56 @@ impl std::ops::Mul<Matrix> for Matrix {
     }
 }
 
+impl std::ops::Mul<Vector> for Matrix {
+    type Output = Vector;
+
+    fn mul(self, rhs: Vector) -> Self::Output {
+        Vector::new(
+            (self[0][0] * rhs.x) + (self[0][1] * rhs.y) + (self[0][2] * rhs.z),
+            (self[1][0] * rhs.x) + (self[1][1] * rhs.y) + (self[1][2] * rhs.z),
+            (self[2][0] * rhs.x) + (self[2][1] * rhs.y) + (self[2][2] * rhs.z),
+        )
+    }
+}
+
+impl std::ops::Mul<Point> for Matrix {
+    type Output = Tuple;
+
+    fn mul(self, rhs: Point) -> Self::Output {
+        tup![
+            (self[0][0] * rhs.x) + (self[0][1] * rhs.y) + (self[0][2] * rhs.z) + self[0][3],
+            (self[1][0] * rhs.x) + (self[1][1] * rhs.y) + (self[1][2] * rhs.z) + self[1][3],
+            (self[2][0] * rhs.x) + (self[2][1] * rhs.y) + (self[2][2] * rhs.z) + self[2][3],
+            self[3][0] + self[3][1] + self[3][2] + self[3][3]
+        ]
+    }
+}
+
+impl std::ops::Mul<Tuple> for Matrix {
+    type Output = Tuple;
+
+    fn mul(self, rhs: Tuple) -> Self::Output {
+        tup![
+            (self[0][0] * rhs[0])
+                + (self[0][1] * rhs[1])
+                + (self[0][2] * rhs[2])
+                + (self[0][3] * rhs[3]),
+            (self[1][0] * rhs[0])
+                + (self[1][1] * rhs[1])
+                + (self[1][2] * rhs[2])
+                + (self[1][3] * rhs[3]),
+            (self[2][0] * rhs[0])
+                + (self[2][1] * rhs[1])
+                + (self[2][2] * rhs[2])
+                + (self[2][3] * rhs[3]),
+            (self[3][0] * rhs[0])
+                + (self[3][1] * rhs[1])
+                + (self[3][2] * rhs[2])
+                + (self[2][3] * rhs[3])
+        ]
+    }
+}
+
 impl std::ops::Index<usize> for Matrix {
     type Output = [f32; 4];
     fn index(&self, index: usize) -> &Self::Output {
@@ -73,7 +125,7 @@ impl std::ops::IndexMut<usize> for Matrix {
 #[cfg(test)]
 mod tests {
 
-    use crate::{mat, matrix::Matrix, point::Point, vector::Vector};
+    use crate::{mat, matrix::Matrix, point::Point, tup, tuple::Tuple, vector::Vector};
 
     #[test]
     fn two_matrices_approx_eq() {
@@ -104,7 +156,33 @@ mod tests {
 
         assert_ne!(matrix1, matrix2)
     }
-
+    #[test]
+    fn mult_matrix_by_vector() {
+        let matrix = mat![1.0, 2.0, 3.0, 4.0;
+                          2.0, 4.0, 4.0, 2.0;
+                          8.0, 6.0, 4.0, 1.0;
+                          0.0, 0.0, 0.0, 1.0];
+        let vector = Vector::new(1.0, 2.0, 3.0);
+        assert_eq!(matrix * vector, Vector::new(14.0, 22.0, 32.0));
+    }
+    #[test]
+    fn mult_matrix_by_point() {
+        let matrix = mat![1.0, 2.0, 3.0, 4.0;
+                          2.0, 4.0, 4.0, 2.0;
+                          8.0, 6.0, 4.0, 1.0;
+                          0.0, 0.0, 0.0, 1.0];
+        let point = Point::new(1.0, 2.0, 3.0);
+        assert_eq!(matrix * point, tup![18.0, 24.0, 33.0, 1.0]);
+    }
+    #[test]
+    fn mult_matrix_by_tuple() {
+        let matrix = mat![1.0, 2.0, 3.0, 4.0;
+                          2.0, 4.0, 4.0, 2.0;
+                          8.0, 6.0, 4.0, 1.0;
+                          0.0, 0.0, 0.0, 1.0];
+        let tuple = tup![1.0, 2.0, 3.0, 1.0];
+        assert_eq!(matrix * tuple, tup![18.0, 24.0, 33.0, 1.0]);
+    }
     #[test]
     fn access_matrix_by_index() {
         let mut matrix = mat![1.0, 2.0, 3.0, 4.0;
