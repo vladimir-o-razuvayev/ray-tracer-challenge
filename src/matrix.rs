67a5c7f0469e86b884ce08c5b7083430691e1e59
Vladimir Razuvayev <vladimir.o.razuvayev@gmail.com>
2022-10-02 12:36:57 -0400

    matrix refactor

--- src/matrix.rs
+++ src/matrix.rs
@@ -1,77 +1,71 @@
-use crate::tuple::Tuple;
+use float_cmp::approx_eq;
 
-pub(crate) enum MixedTuples {
-    TwoTuple(Tuple, Tuple),
-    ThreeTuple(Tuple, Tuple, Tuple),
-    FourTuple(Tuple, Tuple, Tuple, Tuple),
+#[macro_export]
+macro_rules! mat {
+    () => (
+        Matrix::zero()
+    );
+    ($( $( $x: expr ),*);*) => (
+        Matrix::new([ $( [ $($x),* ] ),* ])
+    );
 }
 
-impl From<(Tuple, Tuple)> for MixedTuples {
-    fn from((a, b): (Tuple, Tuple)) -> MixedTuples {
-        MixedTuples::TwoTuple(a, b)
-    }
+#[derive(Debug, Copy, Clone)]
+pub struct Matrix {
+    pub data: [[f32; 4]; 4],
 }
-
-impl From<(Tuple, Tuple, Tuple)> for MixedTuples {
-    fn from((a, b, c): (Tuple, Tuple, Tuple)) -> MixedTuples {
-        MixedTuples::ThreeTuple(a, b, c)
-    }
-}
-
-impl From<(Tuple, Tuple, Tuple, Tuple)> for MixedTuples {
-    fn from((a, b, c, d): (Tuple, Tuple, Tuple, Tuple)) -> MixedTuples {
-        MixedTuples::FourTuple(a, b, c, d)
-    }
-}
-
-#[derive(Debug)]
-pub(crate) struct Matrix {
-    width: usize,
-    height: usize,
-    data: Vec<Tuple>,
-}
-
 impl Matrix {
-    pub fn new<A>(data: A) -> Self
-    where
-        A: Into<MixedTuples>,
-    {
-        match data.into() {
-            MixedTuples::TwoTuple(a, b) => Matrix {
-                width: 2,
-                height: 2,
-                data: vec![a, b],
-            },
-            MixedTuples::ThreeTuple(a, b, c) => Matrix {
-                width: 3,
-                height: 3,
-                data: vec![a, b, c],
-            },
-            MixedTuples::FourTuple(a, b, c, d) => Matrix {
-                width: 4,
-                height: 4,
-                data: vec![a, b, c, d],
-            },
-        }
+    pub fn new(data: [[f32; 4]; 4]) -> Self {
+        Matrix { data }
+    }
+
+    pub fn zero() -> Self {
+        mat![0.0, 0.0, 0.0, 0.0;
+             0.0, 0.0, 0.0, 0.0;
+             0.0, 0.0, 0.0, 0.0;
+             0.0, 0.0, 0.0, 0.0]
     }
 }
 
 impl PartialEq for Matrix {
     fn eq(&self, rhs: &Matrix) -> bool {
-        (self[0] == rhs[0]) && (self[1] == rhs[1]) && (self[2] == rhs[2]) && (self[3] == rhs[3])
+        for row in 0..4 {
+            for col in 0..4 {
+                if !approx_eq!(f32, self[row][col], rhs[row][col]) {
+                    return false;
+                }
+            }
+        }
+        true
+    }
+}
+
+impl std::ops::Mul<Matrix> for Matrix {
+    type Output = Matrix;
+
+    fn mul(self, rhs: Matrix) -> Self::Output {
+        let mut matrix = Matrix::zero();
+        for row in 0..4 {
+            for col in 0..4 {
+                matrix[row][col] = self[row][0] * rhs[0][col]
+                    + self[row][1] * rhs[1][col]
+                    + self[row][2] * rhs[2][col]
+                    + self[row][3] * rhs[3][col];
+            }
+        }
+        matrix
     }
 }
 
 impl std::ops::Index<usize> for Matrix {
-    type Output = Tuple;
-
+    type Output = [f32; 4];
     fn index(&self, index: usize) -> &Self::Output {
         &self.data[index]
     }
 }
 
 impl std::ops::IndexMut<usize> for Matrix {
-    fn index_mut(&mut self, index: usize) -> &mut Tuple {
+    fn index_mut(&mut self, index: usize) -> &mut [f32; 4] {
         &mut self.data[index]
     }
 }
@@ -79,54 +73,44 @@ impl std::ops::IndexMut<usize> for Matrix {
 #[cfg(test)]
 mod tests {
 
-    use crate::{matrix::Matrix, point::Point, tup, tuple::Tuple, vector::Vector};
+    use crate::{mat, matrix::Matrix, point::Point, vector::Vector};
 
     #[test]
     fn two_matrices_approx_eq() {
-        let matrix1 = Matrix::new((
-            tup![1.0, 2.0, 3.0, 4.0],
-            tup![5.5, 6.5, 7.5, 8.5],
-            tup![9.0, 10.0, 11.0, 12.0],
-            tup![13.5, 14.5, 15.5, 16.5],
-        ));
+        let matrix1 = mat![1.0, 2.0, 3.0, 4.0;
+                           5.5, 6.5, 7.5, 8.5;
+                           9.0, 10.0, 11.0, 12.0;
+                           13.5, 14.5, 15.5, 16.5];
 
-        let matrix2 = Matrix::new((
-            tup![1.0, 2.0, 3.0, 4.0] * 0.9999999,
-            tup![5.5, 6.5, 7.5, 8.5] * 1.0000001,
-            tup![9.0, 10.0, 11.0, 12.0],
-            tup![13.5, 14.5, 15.5, 16.5],
-        ));
+        let matrix2 = mat![0.9999999, 2.0, 3.0, 4.0;
+                           5.5, 6.5, 7.5, 8.5;
+                           9.0, 10.0, 11.0, 12.0;
+                           13.5, 14.5, 15.5, 16.5];
 
         assert_eq!(matrix1, matrix2)
     }
 
     #[test]
     fn two_matrices_approx_ne() {
-        let matrix1 = Matrix::new((
-            tup![1.0, 2.0, 3.0, 4.0],
-            tup![5.5, 6.5, 7.5, 8.5],
-            tup![9.0, 10.0, 11.0, 12.0],
-            tup![13.5, 14.5, 15.5, 16.5],
-        ));
+        let matrix1 = mat![1.0, 2.0, 3.0, 4.0;
+                           5.5, 6.5, 7.5, 8.5;
+                           9.0, 10.0, 11.0, 12.0;
+                           13.5, 14.5, 15.5, 16.5];
 
-        let matrix2 = Matrix::new((
-            tup![1.0, 2.0, 3.0, 4.0] * 0.5,
-            tup![5.5, 6.5, 7.5, 8.5],
-            tup![9.0, 10.0, 11.0, 12.0],
-            tup![13.5, 14.5, 15.5, 16.5],
-        ));
+        let matrix2 = mat![0.999, 2.0, 3.0, 4.0;
+                           5.5, 6.5, 7.5, 8.5;
+                           9.0, 10.0, 11.0, 12.0;
+                           13.5, 14.5, 15.5, 16.5];
 
         assert_ne!(matrix1, matrix2)
     }
 
     #[test]
     fn access_matrix_by_index() {
-        let mut matrix = Matrix::new((
-            tup![1.0, 2.0, 3.0, 4.0],
-            tup![5.5, 6.5, 7.5, 8.5],
-            tup![9.0, 10.0, 11.0, 12.0],
-            tup![13.5, 14.5, 15.5, 16.5],
-        ));
+        let mut matrix = mat![1.0, 2.0, 3.0, 4.0;
+                              5.5, 6.5, 7.5, 8.5;
+                              9.0, 10.0, 11.0, 12.0;
+                              13.5, 14.5, 15.5, 16.5];
 
         assert_eq!(matrix[3][3], 16.5);
 
